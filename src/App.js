362974fb694264fae96b9e1a73ed30dddf08db80
Puneet Gupta <pmt665@gmail.com>
2021-05-23 12:00:10 +0200

    implement signup functionality

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import SignUpFormContainer from './containers/SignUpForm' ;
+import './components/styles.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+          <h4>SignUp User</h4>
+          <SignUpFormContainer/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/SignUpForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import SignUpForm from '../components/SignUpForm';
+import Users from '../components/Users';
+import { addUser }  from  '../services/userService'
+import Errors  from './../components/Errors'
+import { Spinner } from 'react-bootstrap';
+
+const SignUpFormContainer = () => {
+    const [isTimeOutCompeleted, setTimeOutCompleted] = useState(false);
+    const [showLoading, setShowLoading]= useState(false);
+    const [isError, setError] = useState(false)
+    const sleep = (milliseconds) => {
+        return new Promise((resolve) => setTimeout(resolve, milliseconds));
+    };
+    const handleRegister = async(props) => {
+        const { user } = props;
+        setTimeOutCompleted(false)
+        setShowLoading(true);
+        try {
+            await addUser(user) //Post form data 
+            await sleep(4000) //wait 4 sec
+            setTimeOutCompleted(true); //sets the value and in render calls <users> component which reterive users.
+            setShowLoading(false);
+        }
+        catch(e){
+            setError(true)
+            setShowLoading(false);
+        }
+    }
+    return(
+        <div>
+            { isError 
+            ? <Errors errors ={[ 'some Error occured....Please retry after sometime' ]}/>
+            : <div>
+                    <SignUpForm handleSubmit = {handleRegister} />
+                    {isTimeOutCompeleted &&  <Users/>}
+                    {showLoading && <Spinner animation="border" role="status"/>}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default SignUpFormContainer;
